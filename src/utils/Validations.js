502bfd29aba6bb761529aa308c385ebaa6080444
Kevin Leon <91386910+k-alex-leon@users.noreply.github.com>
2024-12-26 11:27:44 -0500

    feat: new formats and validations

--- src/utils/Validations.js
+++ src/utils/Validations.js
@@ -16,7 +16,46 @@ export function validateInvoice(company, client) {
 }
 
 export function timestampToDate(timestamp) {
-  if(!timestamp) return '';
-  const date = new Timestamp(timestamp.seconds, timestamp.nanoseconds).toDate().toLocaleDateString();;
+  if (!timestamp) return "";
+  const date = new Timestamp(timestamp.seconds, timestamp.nanoseconds)
+    .toDate()
+    .toLocaleDateString();
   return date;
 }
+
+// it gets product atributtes and validates them
+export function validateProduct({
+  name,
+  boxPrice,
+  boxQuantity,
+  price,
+  quantity,
+}) {
+  if (!name || name === "") {
+    notify_warning("Por favor ingrese un nombre de producto");
+    return false;
+  }
+  if ((!boxQuantity || boxQuantity <= 0) && (!quantity || quantity <= 0)) {
+    notify_warning("Por favor ingrese una cantidad válida");
+    return false;
+  }
+
+  if (boxQuantity > 0 && (!boxPrice || boxPrice <= 0)) {
+    notify_warning("Por favor ingrese un precio de caja válido");
+    return false;
+  }
+
+  if (quantity > 0 && (!price || price <= 0)) {
+    notify_warning("Por favor ingrese un precio válido");
+    return false;
+  }
+
+  return true;
+}
+
+export const numberFormat = (value) => {
+  return new Intl.NumberFormat("es-CO", {
+    style: "currency",
+    currency: "COP",
+  }).format(value);
+};
